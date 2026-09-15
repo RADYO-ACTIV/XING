881f2205fad9ebad8b6f35d0266e742cb913b002
RADYO-ACTIV <msee31596@gmail.com>
2026-09-15 21:37:52 +0100

    testing the backend5

--- backend/sources/routes.py
+++ backend/sources/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
-# from .generator import response
-# from schema import QuestionsOutput
+from sources.generator import response
+from sources.schema import QuestionsOutput
 
 router = APIRouter(
     prefix="/questions",
@@ -12,30 +12,3 @@ def get_users():
     return {"status": "active",
       "ready to work": "true"
     }
-
-# @router.post("/get/")
-# def get_user(track: str, difficulty: str):
-#   prompt=f'generate questions on history with difficulty level hard'
-  
-#   try:
-#     result, error = response(question=prompt, output_schema=QuestionOutput, instructions=None)
-#     if data:
-#       return{
-#       "status": "success",
-#       "data": data}
-#     elif error:
-#       raise HTTPException(
-#         status_code=getattr(error, "code", 440),
-#         detail={
-#           "status_code": getattr(error, "code", 440),
-#           "message": getattr(error, "message", str(error))
-#           })
-  
-#   except HTTPException as exc:
-#     raise exc
-  
-#   except Exception as err:
-#     raise HTTPException(
-#       status_code=500,
-#       detail="an unknown error occoured")
-    
