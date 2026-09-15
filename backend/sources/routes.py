c780408f0ad7507a47114fa485632e54fa5e8a39
RADYO-ACTIV <msee31596@gmail.com>
2026-09-15 21:19:59 +0100

    testing the backend3

--- backend/sources/routes.py
+++ backend/sources/routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from .generator import response
+# from .generator import response
 from schema import QuestionsOutput
 
 router = APIRouter(
@@ -13,29 +13,29 @@ def get_users():
       "ready to work": "true"
     }
 
-@router.post("/get/")
-def get_user(track: str, difficulty: str):
-  prompt=f'generate questions on history with difficulty level hard'
+# @router.post("/get/")
+# def get_user(track: str, difficulty: str):
+#   prompt=f'generate questions on history with difficulty level hard'
   
-  try:
-    result, error = response(question=prompt, output_schema=QuestionOutput, instructions=None)
-    if data:
-      return{
-      "status": "success",
-      "data": data}
-    elif error:
-      raise HTTPException(
-        status_code=getattr(error, "code", 440),
-        detail={
-          "status_code": getattr(error, "code", 440),
-          "message": getattr(error, "message", str(error))
-          })
+#   try:
+#     result, error = response(question=prompt, output_schema=QuestionOutput, instructions=None)
+#     if data:
+#       return{
+#       "status": "success",
+#       "data": data}
+#     elif error:
+#       raise HTTPException(
+#         status_code=getattr(error, "code", 440),
+#         detail={
+#           "status_code": getattr(error, "code", 440),
+#           "message": getattr(error, "message", str(error))
+#           })
   
-  except HTTPException as exc:
-    raise exc
+#   except HTTPException as exc:
+#     raise exc
   
-  except Exception as err:
-    raise HTTPException(
-      status_code=500,
-      detail="an unknown error occoured")
+#   except Exception as err:
+#     raise HTTPException(
+#       status_code=500,
+#       detail="an unknown error occoured")
     
