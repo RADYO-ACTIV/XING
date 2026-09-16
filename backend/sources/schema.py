619fb93f3097cb76d88fe548d01728405b3a59f7
RADYO-ACTIV <msee31596@gmail.com>
2026-09-16 09:16:58 +0100

    testing the backend8

--- backend/sources/schema.py
+++ backend/sources/schema.py
@@ -1,15 +1,21 @@
 from pydantic import BaseModel, ValidationError, Field, field_validator
 from typing import Any, Type, List
 
+class Options(BaseModel):
+  option: str=Field(description='options')
+
+class Topicss(BaseModel):
+  topic: str=Field(description='topics')
+
 class QuestionStructure(BaseModel):
   question: str=Field(description='question')
-  options: List[str]
+  options: List[Options]
   correct: int=Field(description='index of the correct option')
   
 class QuestionsOutput(BaseModel):
   questions: List[QuestionStructure]
 
 class InputStructure(Basemodel):
-  difficulty: str=Field(description='difficulty level of the generated questions'),
-  topic: List[str],
+  difficulty: str=Field(description='difficulty level of the generated questions')
+  topic: List[Topicss]
   questions: int=Field(description='number of questions to generate')
